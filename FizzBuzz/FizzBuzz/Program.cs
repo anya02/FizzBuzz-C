cc2b5afaf54df5255590a9fd124de2e1de905ee7
Anya Murphy <anya.murphy@mac.com>
2022-02-01 16:29:03 +0000

    Can output Fizz, Buzz, Bang, Bong

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,8 +1,29 @@
 ﻿Console.WriteLine("Hello, World!");
 
-for (int i = 0; i < 100; i++)
+for (int i = 1; i < 256; i++)
 {
     string output = string.Empty;
-    output = (i + 1).ToString();
+
+    if (i % 3 == 0)
+    {
+        output = "Fizz";
+    }
+    if (i % 5 == 0)
+    {
+        output += "Buzz";
+    }
+    if (i % 7 == 0)
+    {
+        output += "Bang";
+    }
+    if (i % 11 == 0)
+    {
+        output = "Bong";
+    }
+    if (output.Length < 1)
+    {
+        output = i.ToString();
+    }
+
     Console.WriteLine(output);
 }
